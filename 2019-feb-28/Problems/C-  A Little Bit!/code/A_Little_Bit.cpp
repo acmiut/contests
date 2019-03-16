b0b5e351b3e1e2a780e53750eb848e7ba170891a
maryam97 <marmegh@gmail.com>
2019-03-16 23:55:04 +0330

    fix problems

---
+++ 2019-feb-28/Problems/C-  A Little Bit!/code/A_Little_Bit.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <cstdio>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include <sstream>
+#include <numeric>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <utility>
+#define sz(a) ((int)(a).size())
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+#define Rep(i,j,k) for (int i=(j); i<=(k); i++)
+#define Repd(i,j,k) for (int i=(j); i>=(k); i--)
+#define ALL(c) (c).begin(),(c).end()
+#define TR(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define SUM(a) accumulate(all(a),string())
+#define online1
+#define RAND ((rand()<<15)+rand())
+using namespace std;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> II;
+typedef long long LL;
+
+string s;
+
+int main(){	
+	cin>>s;
+	
+	Rep(i,0,sz(s)-1)
+		if (s[i]=='0'){
+			s.erase(s.begin()+i);
+			cout<<s<<endl;
+			return 0;
+		}
+		
+	s.erase(s.begin());
+	cout<<s<<endl;
+
+    return 0;
+}
